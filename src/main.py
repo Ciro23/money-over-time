95f7396507deaffd0d04ee8c2c2490393f93532e
ciro23 <ciro.sorre23@gmail.com>
2023-12-02 23:56:48 +0100

    fix argparse configuration

--- src/main.py
+++ src/main.py
@@ -11,24 +11,40 @@ class Main:
         self.args = self.parser.parse_args()
 
     def add_arguments(self):
-        self.parser.add_argument("file_path", type=str, help="CSV containing all transactions path")
-        self.parser.add_argument("date_format", type=str, nargs="?",
-                                 help="Date format used in the CSV file, default \"%d/%m/%Y\"")
-        self.parser.add_argument("date_label", type=str, nargs="?",
-                                 help="Date label used in the CSV file, default \"date\"")
-        self.parser.add_argument("amount_label", type=str, nargs="?",
-                                 help="Amount label used in the CSV file, default \"amount\"")
+        self.parser.add_argument(
+            "--file",
+            type=str,
+            help="CSV containing all transactions path"
+        )
+        self.parser.add_argument(
+            "--date_format",
+            type=str,
+            nargs="?",
+            help="Date format used in the CSV file, default \"%%d/%%m/%%Y\""
+        )
+        self.parser.add_argument(
+            "--date_label",
+            type=str,
+            nargs="?",
+            help="Date label used in the CSV file, default \"date\""
+        )
+        self.parser.add_argument(
+            "--amount_label",
+            type=str,
+            nargs="?",
+            help="Amount label used in the CSV file, default \"amount\""
+        )
 
     def execute_program(self):
-        money = MoneyOverTime(self.args.file_path, self.args.date_format, self.args.date_label, self.args.amount_label)
+        money = MoneyOverTime(self.args.file, self.args.date_format, self.args.date_label, self.args.amount_label)
 
         try:
             money_per_time = money.get_money_per_time()
         except FileNotFoundError:
             print("File not found!")
             return
-        except ValueError:
-            print("Date or amount label are not correct!")
+        except ValueError as e:
+            print("Date or amount label are not correct!", e)
             return
 
         plt.plot(money_per_time.keys(), money_per_time.values())
