407ff20759d14b3dd2f647d4ac0016888adfd225
ciro23 <ciro.sorre23@gmail.com>
2024-07-24 11:39:59 +0200

    fix bugs and add unit tests

--- src/MoneyOverTime.py
+++ src/MoneyOverTime.py
@@ -1,5 +1,20 @@
+import csv
 from datetime import datetime
 from typing import Optional
+
+
+def sum_total(entries: dict) -> dict:
+    """
+    Given all movement entries, all the dates are grouped
+    together as keys and the amount of each one is summed
+    as the value.
+    """
+    total = 0
+    for date, amount in entries.items():
+        total += amount
+        entries[date] = round(total, 2)
+
+    return entries
 
 
 class MoneyOverTime:
@@ -30,12 +45,12 @@ class MoneyOverTime:
             "match": skip_value if skip_value is not None else "",
         }
 
-    """
-    Reads all the movements in the specified file and returns a dict
-    with the date of the movement as the key and the sum of the amount
-    of all movements, for that day, as the value.
-    """
     def get_money_per_time(self) -> dict:
+        """
+        Reads all the movements in the specified file and returns a dict
+        with the date of the movement as the key and the sum of the amount
+        of all movements, for that day, as the value.
+        """
         try:
             rows: list = self.__get_lines_of_file()
         except FileNotFoundError as e:
@@ -46,20 +61,20 @@ class MoneyOverTime:
         except ValueError as e:
             raise ValueError(e)
 
-        return self.__sum_total(entries)
+        return sum_total(entries)
 
     def __get_lines_of_file(self) -> list:
         with open(self.file_path, "r", encoding="utf-8-sig") as file:
             return file.read().splitlines()
 
     def __get_row_columns(self, row: str) -> list:
-        return row.split(self.separator)
+        return next(csv.reader([row], delimiter=self.separator))
 
-    """
-    It's necessary to retrieve the index of the "amount" and "date"
-    columns from their label. This method is case-insensitive.
-    """
     def __set_index_of_specific_columns(self, columns: list) -> None:
+        """
+        It's necessary to retrieve the index of the "amount" and "date"
+        columns from their label. This method is case-insensitive.
+        """
         index = 0
 
         for column in columns:
@@ -68,7 +83,10 @@ class MoneyOverTime:
                 self.date['index'] = index
             elif column == self.amount['label'].lower():
                 self.amount['index'] = index
-            elif column == self.skip_label['value'].lower():
+            elif column == self.skip_label['value'].lower() and column != "":
+                # If no row should be skipped, columns containing empty strings must
+                # not be considered, to avoid errors with trailing commas in the csv,
+                # as the default value of self.skip_label['value'] is "".
                 self.skip_label['index'] = index
 
             index += 1
@@ -102,35 +120,23 @@ class MoneyOverTime:
 
             date = columns[self.date['index']]
             amount = columns[self.amount['index']]
+            float_amount = float(amount)
 
             if date in amount_per_date:
-                amount_per_date[date] += float(amount)
+                amount_per_date[date] += float_amount
             else:
-                amount_per_date[date] = float(amount)
+                amount_per_date[date] = float_amount
 
         return self.__sort_by_date_keys(amount_per_date)
 
-    """
-    All movements must be sorted chronologically by the
-    date they were made.
-    """
     def __sort_by_date_keys(self, dictionary: dict) -> dict:
+        """
+        All movements must be sorted chronologically by the
+        date they were made.
+        """
         return dict(
             sorted(
                 dictionary.items(),
                 key=lambda x: datetime.strptime(x[0], self.date['format'])
             )
         )
-
-    """
-    Given all movement entries, all the dates are grouped
-    together as keys and the amount of each one is summed
-    as the value.
-    """
-    def __sum_total(self, entries: dict) -> dict:
-        total = 0
-        for date, amount in entries.items():
-            total += amount
-            entries[date] = total
-
-        return entries
